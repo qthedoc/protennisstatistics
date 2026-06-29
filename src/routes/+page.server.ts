fb2095626f154f850098eca80bb80ccbfef43cbb
Quinn Marsh <wquinnmarsh@gmail.com>
2026-06-29 00:36:46 -0700

    chore: eg 50/50 ATP/WTA

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,6 +2,7 @@ import { getRankings } from '$lib/server/tennis-api';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-	const snapshot = await getRankings('atp');
-	return { topPlayer: snapshot.players[0] };
+	const tour = Math.random() < 0.5 ? 'atp' : 'wta';
+	const snapshot = await getRankings(tour);
+	return { topPlayer: snapshot.players[0], tour };
 };
